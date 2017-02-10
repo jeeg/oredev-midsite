49d52b85ec9f31ee27a7aec9d964bc03ef3b6f18
Fredrik Frodlund <frippz@me.com>
2017-02-10 13:49:58 +0100

    Update ignore file

---
+++ gui/js/main.js
@@ -0,0 +1,2 @@
+"querySelector"in document&&"classList"in document.createElement("a")&&"addEventListener"in window&&document.querySelector("html").classList.add("js");
+//# sourceMappingURL=main.js.map
